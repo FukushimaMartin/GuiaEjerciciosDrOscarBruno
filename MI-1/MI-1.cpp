0e5034cce6694a80b8eb79eabfb134a33884f0ea
FukushimaMartin <fukushimafederico@hotmail.com>
2017-05-21 12:37:39 -0300

    Create MI-1.cpp

---
+++ MI-1/MI-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/*
+MI-1: Dados dos valores enteros A y B, informar la suma, la resta y el producto.
+Federico Martin Fukushima
+17/05/2017
+*/
+
+int main(){
+ int a, b;
+ 
+ 
+ std::cout<<"ingrese dos valores seguidos de la tecla enter\n";
+ std::cin>>a>>b;
+ 
+ std::cout<<"la suma entre a y b es: "<<a+b<<"\n";
+ std::cout<<"la resta entre a y b es: "<<a-b<<"\n";
+ std::cout<<"el producto entre a y b es: "<<a*b;
+    
+}
